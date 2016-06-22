0696e5a29e24e7125f87e0bba84153aef81186a7
lorajdavis <username@users.noreply.github.com>
2016-06-22 13:34:32 -0400

    Dropdown fix
    Remove duplicate "Department Codes" dropdown

--- frontend/assets/repository_menu.js
+++ frontend/assets/repository_menu.js
@@ -1,4 +1,4 @@
 $(function() {
-  $('li.repo-container ul.dropdown-menu').append('<li><a href="/plugins/jhu_accessions/department_codes">Department Codes</a></li>');
+  $('li.repo-container ul.dropdown-menu li.dropdown-submenu ul.dropdown-menu').append('<li><a href="/plugins/jhu_accessions/department_codes">Department Codes</a></li>');
 
 })
